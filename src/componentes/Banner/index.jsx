032173025e56c8bfb6b19142d239c605ee56d853
tunim73 <antonio.netto973@gmail.com>
2022-09-21 14:06:37 -0300

    ataualizações iniciais e conclusão do formulário

---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,12 @@
+import "./banner.css"
+
+const Banner = () => {
+
+    return(
+        <header className="banner">
+            <img src="src/public/assets/banner.png" alt="Banner do organo" />
+        </header>            
+    )
+}
+
+export default Banner
