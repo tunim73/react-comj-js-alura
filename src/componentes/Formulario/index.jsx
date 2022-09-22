6165f2b066e85cf38410a86f1f30e84d06ce0eab
tunim73 <antonio.netto973@gmail.com>
2022-09-22 00:02:11 -0300

    botao e lista suspensa adicionados ao form

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,15 +1,35 @@
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
+import Botao from "../Botao"
 
 const Formulario = () => {
+
+    const times = [
+        "Programação", 
+        "Front-end",
+        "Data Science"
+    ]
+
+    const aoSalvar  = (event)=>{
+        event.preventDefault();
+        
+    }
+
+
     return(
- 
             <section className="formulario"  >               
-               <form>
+               <form onSubmit={aoSalvar}>
                    <h2 className="">Preencha os dados para criar o card do colaborador</h2> 
-                   <CampoTexto label ="Nome" placeholder = "Digite seu nome"/>
-                   <CampoTexto label ="Cargo" placeholder = "Digite seu cargo"/>
+                   <CampoTexto obrigatorio = {true} label ="Nome" placeholder = "Digite seu nome"/>
+                   <CampoTexto obrigatorio = {true} label ="Cargo" placeholder = "Digite seu cargo"/>
                    <CampoTexto label ="Imagem" placeholder = "Digite a URL da imagem"/>
+                   <ListaSuspensa obrigatorio = {true} label ="Time" itens = {times} />
+                   <Botao>
+                        criar card
+                   </Botao>
+
+
                </form>
            </section>
         
